3833a8155ce7d7115af2a448da3ba435ce1fb528
Swati Sharma <41550990+swatisharma27@users.noreply.github.com>
2026-09-05 15:18:51 -0700

    Add: lowest-common-ancestor-in-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-2.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# class Solution:
+#     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+#         """
+#         Only BST 
+#         TC: O(h)
+#         SC: O(h)
+#         """
+
+#         # base
+#         if root is None:
+#             return None
+
+#         # logic
+#         if root.val > p.val and root.val > q.val:
+#             return self.lowestCommonAncestor(root.left, p , q)
+#         elif root.val < p.val and root.val < q.val:
+#             return self.lowestCommonAncestor(root.right, p , q)
+#         else:
+#             return root
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        """
+        Both BST and BT
+        TC: O(n)
+        SC: O(h)
+        """
+        # base
+        if root is None or p == root or q == root:
+            return root
+
+        # logic
+        left = self.lowestCommonAncestor(root.left, p , q)
+        right = self.lowestCommonAncestor(root.right, p , q)
+
+        if left == None and right == None:
+            return None
+        elif left != None and right == None:
+            return left
+        elif left == None and right != None:
+            return right
+        else:
+            return root
+
